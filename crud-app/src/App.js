f457326ebb60308d2a08e1a784dab523094d5890
MarlonDValencia <marlondvalencia@gmail.com>
2021-11-24 20:13:15 -0500

    Creada tabla y validacion users

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, {useState} from 'react'
+import UserTable from './components/UserTable';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
+  const userData = [
+    {id: uuidv4() , name: "Tania", username:"floppydiskette"},
+    {id: uuidv4() , name: "Craig", username:"siliconeidolon"},
+    {id: uuidv4() , name: "Ben", username:"benisphere"},
+  ]
+
+  const [users, setUsers] = useState(userData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <h1>CRUD App with hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+      <div className="flex-large">
+        <h2>View Users</h2>
+        <UserTable users={users}/>
+      </div>
+      </div>
     </div>
+    </>
   );
 }
 
